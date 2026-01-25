7819720b4173a96f0ab97f1dab6c574342bdcdaf
DaltonGorham <daltongorham@gmail.com>
2026-01-25 13:26:08 -0600

    move util implementation out of header file, to follow cpp coding standards

---
+++ src/Utilities.cpp
@@ -0,0 +1,87 @@
+#include "../include/Utilities.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <filesystem>
+
+void Utilities::validateArgs(int argc, char* argv[], int expectedArgs) {
+    validateNumberOfArgs(argc, argv, expectedArgs);
+    validateArgTypes(argv);
+}
+
+void Utilities::validateNumberOfArgs(int argc, char* argv[], int expectedArgs) {
+    if (argc != expectedArgs) {
+        std::cerr << "Usage: " << argv[0]
+            << " <input_file>"
+            << " <number_of_clusters>"
+            << " <max_iterations>"
+            << " <convergence_threshold>"
+            << " <number_of_runs>\n";
+        std::exit(1);
+    }
+}
+
+void Utilities::validateArgTypes(char* argv[]) {
+    if (!validateInputFile(argv[INPUT_FILE])) {
+        std::cerr << "Error: Input file: " << "`" << argv[INPUT_FILE] << "` does not exist relative to the executable.\n";
+        std::cerr << "Error: Please provide a valid file path or place the file in the correct directory.\n";
+        std::exit(1);
+    }
+    if (!validateClusters(argv[NUMBER_OF_CLUSTERS])) {
+        std::cerr << "number_of_clusters: " << "`" << argv[NUMBER_OF_CLUSTERS] << "` must be an integer greater than 1.\n";
+        std::exit(1);
+    }
+    if (!validateIterations(argv[MAX_ITERATIONS])) {
+        std::cerr << "max_iterations: " << "`" << argv[MAX_ITERATIONS] << "` must be a positive integer.\n";
+        std::exit(1);
+    }
+    if (!validateThreshold(argv[CONVERGENCE_THRESHOLD])) {
+        std::cerr << "convergence_threshold: " << "`" << argv[CONVERGENCE_THRESHOLD] << "` must be a positive real number.\n";
+        std::exit(1);
+    }
+    if (!validateRuns(argv[NUMBER_OF_RUNS])) {
+        std::cerr << "number_of_runs: " << "`" << argv[NUMBER_OF_RUNS] << "` must be a positive integer.\n";
+        std::exit(1);
+    }
+}
+
+bool Utilities::validateInputFile(const char* arg) {
+    return std::filesystem::exists(arg);
+}
+
+bool Utilities::validateClusters(char* arg) {
+    std::string str = arg;
+    for (const auto& c : str) {
+        if (!std::isdigit(c)) return false;
+    }
+    return std::stoi(str) > 1;
+}
+
+bool Utilities::validateIterations(char* arg) {
+    std::string str = arg;
+    for (const auto& c : str) {
+        if (!std::isdigit(c)) return false;
+    }
+    return std::stoi(str) > 0;
+}
+
+bool Utilities::validateThreshold(char* arg) {
+    std::string str = arg;
+    size_t pos;
+    float threshold;
+    try {
+        threshold = std::stof(str, &pos);
+        if (pos != str.length()) return false;
+    } catch (...) {
+        return false;
+    }
+    return threshold > 0.0f;
+}
+
+bool Utilities::validateRuns(char* arg) {
+    std::string str = arg;
+    for (const auto& c : str) {
+        if (!std::isdigit(c)) return false;
+    }
+    return std::stoi(str) > 0;
+}
